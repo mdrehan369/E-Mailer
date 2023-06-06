74b01562b5f9f92528bb71427bad7834220465c8
MD REHAN <mdrehan4650@gmail.com>
2023-06-05 23:36:46 -0700

    pushing the repository to remote

--- GUIclass.py
+++ GUIclass.py
@@ -1,2 +1,64 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
+class GUIcustom(tk.Tk):
+
+    def __init__(self, title, width, height, resizable = True):
+
+        super().__init__()
+        # self.title = title
+        self.width = width
+        self.height = height
+
+        self.title(title)
+        self.geometry(f"{width}x{height}")
+
+        if(resizable == False):
+            self.resizable(0,0)
+
+    def getPixels(self, percent, what):
+
+        if(percent == 0):
+            return None
+
+        if(what == "width"):
+            return (percent / 100) * self.width
+        
+        else:
+            return (percent / 100) * self.height
+
+    def setImage(self, imageFile, resize):
+
+        rawImage = Image.open(imageFile)
+        rawImage = rawImage.resize(resize)
+        image = ImageTk.PhotoImage(image = rawImage)
+
+        return image
+        
+    def createFrame(self, master, side, packpropagate = False, expand= False, height= 0, width= 0, fill= "none", padx= 0, pady= 0, anchor= "center", bg= None):
+
+        frame = tk.Frame(master = master, bg = bg, height=self.getPixels(height, "height"), width = self.getPixels(width, "width"))
+        frame.pack(side = side, fill = fill, expand=expand, padx=padx, pady= pady, anchor=anchor)
+
+        if(packpropagate == True):
+            frame.pack_propagate(0);
+
+        return frame
+
+    def createLabel(self, master, side, fill= "none", expand= False, text= None, image= None, resize= (0,0), padx= 0, pady= 0, anchor= "center", bg= None, width=0, height=0, font="lucida"):
+
+        img = None
+        if(image != None):
+            img = self.setImage(image, resize)
+
+        label = tk.Label(master= master, bg = bg, text= text, image= img, width=width, height=height, font=font)
+        label.pack(side=side, fill= fill, expand= expand, padx=padx, pady= pady, anchor=anchor)
+
+        return label
+
+    def createEntry(self, master, side, padx= 0, pady= 0, anchor= "center", width=0, textvar= None, fill="none", expand= False, ipadx=0, ipady= 0, font= "lucida"):
+
+        entry = tk.Entry(master=master, textvariable=textvar, width=width, font=font)
+        entry.pack(side=side, padx=padx, pady= pady, anchor=anchor, fill=fill, expand=expand, ipady=ipady, ipadx= ipadx)
+
+        return entry
--- main.py
+++ main.py
@@ -1,34 +1,58 @@
-import tkinter as t
-from PIL import Image, ImageTk
+# import tkinter as t
+# from PIL import Image, ImageTk
 
 
-#basic root window
-root = t.Tk()
-root.geometry("744x546")
-root.title("E-Mailer")
-root.resizable(0,0)
+# #basic root window
+# root = t.Tk()
+# root.geometry("744x546")
+# root.title("E-Mailer")
+# root.resizable(0,0)
 
-#header frame
-header = t.Frame(root, background="red", height=100)
-header.pack(side="top", fill="x")
-header.pack_propagate(0)
+# #header frame
+# header = t.Frame(root, background="red", height=100)
+# header.pack(side="top", fill="x")
+# header.pack_propagate(0)
 
-#setting logo
-image = Image.open("basic design.png")
-image = image.resize((100, 50))
-img = ImageTk.PhotoImage(image=image)
-logo = t.Label(header, image=img)
-logo.pack(side="left", anchor="w")
+# #setting logo
+# image = Image.open("basic design.png")
+# image = image.resize((100, 50))
+# img = ImageTk.PhotoImage(image=image)
+# logo = t.Label(header, image=img)
+# logo.pack(side="left", anchor="w")
 
-# setting heading
-heading = t.Label(header, text= "E-Mailer", font="20", bg="red")
-heading.pack(side="left", anchor="center", fill="both", expand=True)
+# # setting heading
+# heading = t.Label(header, text= "E-Mailer", font="20", bg="red")
+# heading.pack(side="left", anchor="center", fill="both", expand=True)
 
-#Left side frame
-leftFrame = t.Frame(root, bg="green", width=372)
-leftFrame.pack(side="left", fill="y")
+# #Left side frame
+# leftFrame = t.Frame(root, bg="green", width=372)
+# leftFrame.pack(side="left", fill="y")
 
-rightFrame = t.Frame(root, bg="blue", width=372)
-rightFrame.pack(side="left", fill="y")
+# rightFrame = t.Frame(root, bg="blue", width=372)
+# rightFrame.pack(side="left", fill="y")
+
+# root.mainloop()
+
+from GUIclass import GUIcustom as GUI
+
+root = GUI("E-Mailer", 744, 546, False)
+
+header = root.createFrame(root, bg="red", height=20, side="top", fill="x", packpropagate=True)
+
+logo = root.createLabel(header, image="basic design.png", side="left", anchor="w", resize=(100, 50))
+
+bottomFrame = root.createFrame(root, bg= "yellow", expand=True, fill="x", side="bottom", height=20, packpropagate=True, anchor="s")
+
+leftFrame = root.createFrame(root, bg= "green", width= 50, side= "left", fill="y", height= 60, packpropagate=True)
+rightFrame = root.createFrame(root, bg= "blue", width= 50, side= "left", fill="y", height= 60, packpropagate=True)
+
+fromFrame = root.createFrame(leftFrame, side="top", width= 50, height=20, fill="x", packpropagate=True)
+fromLabel = root.createLabel(fromFrame, side="left", anchor="center", text="From : ", width=12, font="lucida 15")
+fromEntry = root.createEntry(fromFrame, side="right", anchor="center", width=45, ipady=2, padx=5, expand=True, font="lucida 13")
+
+toFrame = root.createFrame(leftFrame, side="top", width= 50, height=20, fill="x", packpropagate=True)
+toLabel = root.createLabel(toFrame, side="left", anchor="center", text="To : ", width=12, font="lucida 15")
+toEntry = root.createEntry(toFrame, side="right", anchor="center", width=45, ipady=2, padx=5, expand=True, font="lucida 13")
+
 
 root.mainloop()
